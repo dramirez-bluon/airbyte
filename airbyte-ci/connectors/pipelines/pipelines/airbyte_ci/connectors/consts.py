d99f86b4dd7ef5b2c39e7f24b1cdad24b0d16929
Catherine Noll <noll.catherine@gmail.com>
2024-04-19 11:59:42 +0100

    Remove custom regression test step

--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/consts.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/consts.py
@@ -30,7 +30,6 @@ class CONNECTOR_TEST_STEP_ID(str, Enum):
     CHECK_MIGRATION_CANDIDATE = "check_migration_candidate"
     POETRY_INIT = "poetry_init"
     DELETE_SETUP_PY = "delete_setup_py"
-    REGRESSION_TEST_BUILD_TARGET = "regression_test_build_target"
     REGRESSION_TESTS = "regression_tests"
 
     def __str__(self) -> str:
