2fdba491bb2d9c37ac77981281d2cd25f6befc51
Catherine Noll <clnoll@users.noreply.github.com>
2024-04-24 13:33:31 -0400

    Update airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/commands.py
    Co-authored-by: Augustin <augustin@airbyte.io>

--- airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/commands.py
+++ airbyte-ci/connectors/pipelines/pipelines/airbyte_ci/connectors/commands.py
@@ -154,7 +154,6 @@ def should_use_remote_secrets(use_remote_secrets: Optional[bool]) -> bool:
         "migrate-to-poetry": "pipelines.airbyte_ci.connectors.migrate_to_poetry.commands.migrate_to_poetry",
         "upgrade_base_image": "pipelines.airbyte_ci.connectors.upgrade_base_image.commands.upgrade_base_image",
         "upgrade_cdk": "pipelines.airbyte_ci.connectors.upgrade_cdk.commands.bump_version",
-        "run_regression_tests": "pipelines.airbyte_ci.connectors.run_regression_tests.commands.test",
     },
 )
 @click.option(
